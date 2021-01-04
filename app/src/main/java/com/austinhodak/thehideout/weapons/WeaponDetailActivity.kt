b9d48aadf8c9adeb021655a83744c6a52b50ed32
Austin Hodak <ahodak65@gmail.com>
2021-01-04 07:30:19 -0500

    Started weapon details.

---
+++ app/src/main/java/com/austinhodak/thehideout/weapons/WeaponDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.austinhodak.thehideout.weapons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.austinhodak.thehideout.R
+
+class WeaponDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.weapon_detail)
+    }
+}
