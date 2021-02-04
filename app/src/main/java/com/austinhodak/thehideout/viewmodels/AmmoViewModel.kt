b4ce113d4e7305c7938fcc9e1d29c4d882b912c7
Austin <ahodak65@gmail.com>
2021-02-04 01:30:33 -0500

    Added new ammo database via Firestore and caching.

--- app/src/main/java/com/austinhodak/thehideout/viewmodels/AmmoViewModel.kt
+++ app/src/main/java/com/austinhodak/thehideout/viewmodels/AmmoViewModel.kt
@@ -1,11 +1,14 @@
 package com.austinhodak.thehideout.viewmodels
 
 import android.app.Application
+import android.content.SharedPreferences
 import android.util.Log
+import androidx.core.content.edit
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
+import androidx.preference.PreferenceManager
 import com.austinhodak.thehideout.R
 import com.austinhodak.thehideout.viewmodels.models.AmmoModel
 import com.austinhodak.thehideout.viewmodels.models.CaliberModel
@@ -24,6 +27,7 @@ import java.lang.reflect.Type
 class AmmoViewModel(application: Application) : AndroidViewModel(application){
 
     private val context = getApplication<Application>().applicationContext
+    val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     val sortBy : LiveData<Int> get() = _sortBy
     private val _sortBy = MutableLiveData<Int>()
@@ -42,8 +46,25 @@ class AmmoViewModel(application: Application) : AndroidViewModel(application){
 
     init {
         setSortBy(0)
-        loadCalibersFirestore()
-        loadAmmoFirestore()
+
+        val hours_4 = 1000 * 60 * 60 * 4
+
+        if ((System.currentTimeMillis() - prefs.getLong("lastLoad", 0)) > hours_4) {
+            //Loaded over 4 hours ago.
+
+            loadCalibersFirestore(Source.CACHE)
+            loadAmmoFirestore(Source.CACHE)
+
+            loadCalibersFirestore(Source.DEFAULT)
+            loadAmmoFirestore(Source.DEFAULT)
+
+            prefs.edit {
+                putLong("lastLoad", System.currentTimeMillis())
+            }
+        } else {
+            loadCalibersFirestore(Source.CACHE)
+            loadAmmoFirestore(Source.CACHE)
+        }
     }
 
     fun setSortBy(int: Int) {
@@ -59,8 +80,8 @@ class AmmoViewModel(application: Application) : AndroidViewModel(application){
         data.value?.find { it._id == id }?.ammo
     }
 
-    private fun loadCalibersFirestore() {
-        Firebase.firestore.collection("calibers").get(Source.CACHE).addOnSuccessListener { docs ->
+    private fun loadCalibersFirestore(source: Source) {
+        Firebase.firestore.collection("calibers").get(source).addOnSuccessListener { docs ->
             Log.d("AMMO", "CALIBERS LOADED")
 
             val list: MutableList<FSCaliber> = ArrayList()
@@ -75,8 +96,8 @@ class AmmoViewModel(application: Application) : AndroidViewModel(application){
         }
     }
 
-    private fun loadAmmoFirestore() {
-        Firebase.firestore.collectionGroup("ammo").get(Source.CACHE).addOnSuccessListener { docs ->
+    private fun loadAmmoFirestore(source: Source) {
+        Firebase.firestore.collectionGroup("ammo").get(source).addOnSuccessListener { docs ->
             Log.d("AMMO", "AMMO LOADED")
 
             val list: MutableList<FSAmmo> = ArrayList()
