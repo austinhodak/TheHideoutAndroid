4e1f35c56280b8c50817d86066f6a5aad05955a6
Austin <ahodak65@gmail.com>
2021-11-29 18:44:55 -0500

    Team management screen mostly complete.

--- app/src/main/java/com/austinhodak/thehideout/team/TeamManagementActivity.kt
+++ app/src/main/java/com/austinhodak/thehideout/team/TeamManagementActivity.kt
@@ -1,10 +1,14 @@
 package com.austinhodak.thehideout.team
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,13 +19,20 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.adapty.models.ProductModel
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.color.ColorPalette
+import com.afollestad.materialdialogs.color.colorChooser
+import com.afollestad.materialdialogs.input.input
 import com.austinhodak.thehideout.GodActivity
 import com.austinhodak.thehideout.R
 import com.austinhodak.thehideout.compose.theme.DarkGrey
+import com.austinhodak.thehideout.compose.theme.DividerDark
 import com.austinhodak.thehideout.compose.theme.HideoutTheme
 import com.austinhodak.thehideout.compose.theme.Red400
 import com.austinhodak.thehideout.firebase.Team
@@ -34,18 +45,17 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.dynamiclinks.ShortDynamicLink
 import com.google.firebase.dynamiclinks.ktx.androidParameters
-import com.google.firebase.dynamiclinks.ktx.dynamicLink
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class TeamManagementActivity : GodActivity() {
 
     private val viewModel: TeamManagementViewModel by viewModels()
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -59,7 +69,7 @@ class TeamManagementActivity : GodActivity() {
                         TopBar()
                     },
                     floatingActionButton = {
-                        ExtendedFloatingActionButton(
+                        /*ExtendedFloatingActionButton(
                             text = {
                                 Text(text = "JOIN TEAM", color = Color.Black)
                             },
@@ -73,7 +83,7 @@ class TeamManagementActivity : GodActivity() {
                                     tint = Color.Black
                                 )
                             }
-                        )
+                        )*/
                     }
                 ) {
                     LazyColumn(
@@ -91,6 +101,7 @@ class TeamManagementActivity : GodActivity() {
         }
     }
 
+    @SuppressLint("CheckResult")
     @Composable
     private fun TeamCard(teamID: String?) {
         var team by remember { mutableStateOf<Team?>(null) }
@@ -99,6 +110,7 @@ class TeamManagementActivity : GodActivity() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 if (snapshot.exists()) {
                     team = snapshot.getValue(Team::class.java)
+                    team?.id = teamID
                 }
             }
 
@@ -115,31 +127,63 @@ class TeamManagementActivity : GodActivity() {
                 backgroundColor = DarkGrey
             ) {
                 Column(
-                    Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    Modifier.padding(top = 16.dp, start = 0.dp, end = 0.dp, bottom = 8.dp)
                 ) {
                     Row(
-                        verticalAlignment = Alignment.CenterVertically
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
                     ) {
                         Text(text = it.name ?: "", style = MaterialTheme.typography.h6)
                         Spacer(modifier = Modifier.weight(1f))
                         if (it.members?.get(uid())?.owner == true) {
-                            IconButton(onClick = { }, Modifier.size(20.dp)) {
-                                Icon(painter = painterResource(id = R.drawable.ic_baseline_edit_24), contentDescription = "Edit Team")
+                            IconButton(onClick = {
+                                MaterialDialog(this@TeamManagementActivity).show {
+                                    title(text = "Change Team Name")
+                                    input(
+                                        prefill = "${it.name}",
+                                        hint = "Team Name"
+                                    ) { materialDialog, charSequence ->
+                                        it.updateName(charSequence.toString())
+                                    }
+                                    positiveButton(text = "SAVE")
+                                    negativeButton(text = "CANCEL")
+                                }
+                            }, Modifier.size(20.dp)) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_baseline_edit_24),
+                                    contentDescription = "Edit Team"
+                                )
                             }
                         }
                     }
 
+                    Divider(color = DividerDark)
+
                     it.members?.forEach {
-                        TeamMemberItem(id = it.key)
+                        TeamMemberItem(id = it.key, it.value, teamID)
                     }
 
+                    Divider(color = DividerDark)
+
                     Row(
-                        Modifier.fillMaxWidth(),
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(end = 16.dp, top = 8.dp),
                         horizontalArrangement = Arrangement.End
                     ) {
-                        TextButton(modifier = Modifier.padding(end = 8.dp), onClick = {
-                            createInviteLink(team, teamID)
-                        }) {
+                        TextButton(
+                            modifier = Modifier.padding(end = 8.dp),
+                            onClick = {
+                                MaterialDialog(this@TeamManagementActivity).show {
+                                    title(text = "Leave Team?")
+                                    message(text = "You cannot undo this action, if you are the only member, the team will be deleted.")
+                                    positiveButton(text = "LEAVE") {
+                                        team?.leave()
+                                    }
+                                    negativeButton(text = "NEVERMIND")
+                                }
+                            }
+                        ) {
                             Text(text = "LEAVE", color = Color.Gray)
                         }
                         TextButton(onClick = {
@@ -153,8 +197,10 @@ class TeamManagementActivity : GodActivity() {
         }
     }
 
+    @SuppressLint("CheckResult")
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
-    private fun TeamMemberItem(id: String) {
+    private fun TeamMemberItem(id: String, value: Team.MemberSettings, teamID: String?) {
         var teamMember by remember { mutableStateOf<User?>(null) }
         questsFirebase.child("users/$id")
             .addListenerForSingleValueEvent(object : ValueEventListener {
@@ -172,16 +218,51 @@ class TeamManagementActivity : GodActivity() {
 
         teamMember?.let {
             Row(
-                Modifier.padding(vertical = 4.dp)
+                Modifier
+                    .height(48.dp)
+                    .combinedClickable(
+                        onClick = {
+                            MaterialDialog(this).show {
+                                title(text = "Your Color")
+                                colorChooser(
+                                    colors = ColorPalette.Primary,
+                                    subColors = ColorPalette.PrimarySub,
+                                    initialSelection = android.graphics.Color.parseColor(value.color)
+                                ) { _, color ->
+                                    value.updateColor("#${Integer.toHexString(color).substring(2)}", teamID!!, id)
+                                }
+                                positiveButton(text = "SAVE")
+                            }
+                        }
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(
-                    "${it.getUsername()}${
-                        if (it.uid == uid()) {
-                            " (You)"
-                        } else ""
-                    }"
-                )
+                Rectangle(color = value.getColorM(), modifier = Modifier.fillMaxHeight())
+                Row(
+                    Modifier
+                        .padding(start = 16.dp, end = 16.dp)
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        "${it.getUsername()}${
+                            if (it.uid == uid()) {
+                                " (You)"
+                            } else ""
+                        }", fontWeight = if (it.uid == uid()) FontWeight.Bold else FontWeight.Normal
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    if (it.uid == uid()) {
+                        Icon(
+                            painterResource(id = R.drawable.icons8_crown_96),
+                            contentDescription = "Owner",
+                            tint = Color.White,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
             }
+
         }
     }
 
@@ -209,6 +290,7 @@ class TeamManagementActivity : GodActivity() {
         startActivity(Intent.createChooser(intent, "Share Link"))
     }
 
+    @SuppressLint("CheckResult")
     @Composable
     private fun TopBar() {
         TopAppBar(
@@ -223,7 +305,36 @@ class TeamManagementActivity : GodActivity() {
             backgroundColor = if (isSystemInDarkTheme()) Color(0xFE1F1F1F) else MaterialTheme.colors.primary,
             actions = {
                 IconButton(onClick = {
+                    MaterialDialog(this@TeamManagementActivity).show {
+                        title(text = "Create New Team")
+                        input(
+                            hint = "Team Name"
+                        ) { materialDialog, charSequence ->
+                            val key = questsFirebase.child("teams").push().key
+                            key?.let {
+                                uid()?.let {
+                                    val newTeam = Team(
+                                        name = charSequence.toString(),
+                                        members = mapOf(
+                                            uid()!! to Team.MemberSettings(
+                                                color = "#F44336",
+                                                owner = true
+                                            )
+                                        )
+                                    )
 
+                                    val childUpdates = hashMapOf(
+                                        "users/${uid()!!}/teams/$key" to true,
+                                        "teams/$key" to newTeam
+                                    )
+
+                                    questsFirebase.updateChildren(childUpdates)
+                                }
+                            }
+                        }
+                        positiveButton(text = "SAVE")
+                        negativeButton(text = "CANCEL")
+                    }
                 }) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_baseline_add_circle_24),
@@ -232,6 +343,19 @@ class TeamManagementActivity : GodActivity() {
                     )
                 }
             }
+        )
+    }
+
+    @Composable
+    fun Rectangle(
+        color: Color,
+        modifier: Modifier = Modifier
+    ) {
+        Box(
+            modifier = modifier
+                .width(2.dp)
+                .clip(RectangleShape)
+                .background(color)
         )
     }
 }
