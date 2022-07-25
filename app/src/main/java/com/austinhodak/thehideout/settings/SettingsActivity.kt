36347c1a8f865337451e5472ebadf85288574341
austinhodak <ahodak65@gmail.com>
2022-07-25 17:53:40 -0400

    Add voting link and discord invite.

--- app/src/main/java/com/austinhodak/thehideout/settings/SettingsActivity.kt
+++ app/src/main/java/com/austinhodak/thehideout/settings/SettingsActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -208,7 +210,11 @@ class SettingsActivity : GodActivity() {
                             },
                             backgroundColor = if (isSystemInDarkTheme()) Color(0xFE1F1F1F) else MaterialTheme.colors.primary,
                             actions = {
-
+                                IconButton(onClick = {
+                                    "https://discord.gg/YQW36z29z6".openWithCustomTab(this@SettingsActivity)
+                                }) {
+                                    Icon(painter = painterResource(id = R.drawable.ic_icons8_discord_svg), contentDescription = null, tint = Color.White)
+                                }
                             }
                         )
                     }
@@ -1079,6 +1085,14 @@ class SettingsActivity : GodActivity() {
                                                 }
                                             }
                                         }
+                                    }
+                                }
+                                button {
+                                    title = "Vote For Features".asText()
+                                    icon = R.drawable.ic_baseline_how_to_vote_24.asIcon()
+                                    enabled = true
+                                    onClick = {
+                                        "https://feedback.thehideout.io".openWithCustomTab(this@SettingsActivity)
                                     }
                                 }
                             }
