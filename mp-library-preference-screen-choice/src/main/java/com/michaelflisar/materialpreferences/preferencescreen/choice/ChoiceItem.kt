abc7d371048c7d89a5b628117a20c12fde7d7154
Austin Hodak <ahodak65@gmail.com>
2023-01-23 23:07:01 -0500

    Remove unused mp-library.

--- mp-library-preference-screen-choice/src/main/java/com/michaelflisar/materialpreferences/preferencescreen/choice/ChoiceItem.kt
+++
@@ -1,9 +0,0 @@
-package com.michaelflisar.materialpreferences.preferencescreen.choice
-
-import com.michaelflisar.text.Text
-
-interface ChoiceItem {
-    val order: Int
-    val label: Text
-    val id: Int
-}
